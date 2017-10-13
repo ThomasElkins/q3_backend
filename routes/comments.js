91dd31e57c3edb65810c01add64bed194f8b288f
ThomasElkins <elkins.thomas@gmail.com>
2017-10-13 04:06:24 -0700

    nice job:

--- routes/comments.js
+++ routes/comments.js
@@ -8,6 +8,20 @@ router.get('/', function(req, res, next) {
     .then(function(comments) {
       res.send(comments.rows)
     })
-});
+})
+
+router.post('/', function(req, res, next) {
+  knex('comments').insert({
+    user_name: req.body.user_name,
+    city: req.body.city,
+    hike_name: req.body.hike_name,
+    comment: req.body.comment
+  }).then(function() {
+    knex('comments').select()
+    .then(function(){
+      res.redirect('/comments')
+    })
+  })
+})
 
 module.exports = router;
