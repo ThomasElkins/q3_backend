70396e0048eedfce5a612c5e7a6ad2e33bef5ae7
ThomasElkins <elkins.thomas@gmail.com>
2017-10-13 07:26:30 -0700

    changed seeds

--- seeds/comments.js
+++ seeds/comments.js
@@ -5,7 +5,8 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('comments').insert([
         {user_name: 'Thomas', city: 'Phoenix', hike_name: `Camelback`, comment: `Crowded on the weekends and holidays`},
-        {user_name: 'Jess', city: 'Phoenix', hike_name: `South Mountain`, comment: `Great Views!`}
+        {user_name: `Jess`, city: `Phoenix`, hike_name: `McDowell Mtn`, comment: `Great Views!`},
+        {user_name: `Mac`, city: `Tucson`, hike_name: `Cactus Forest`, comment: `So many cactus's (or is it cacti?) whatever they are awesome!`}
       ]);
     });
 };
